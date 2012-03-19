2045e70b38dc25b8760b625dd50fc568a7b272b9
VadimsK <Chaim@Chaim-PC.(none)>
2012-03-19 15:54:43 +0100

    READEM.md changed, licence added

--- upload/includes/Indexdepot/Solr/HttpTransport/Curl.php
+++ upload/includes/Indexdepot/Solr/HttpTransport/Curl.php
@@ -1,5 +1,26 @@
 <?php
-
+/**
+ * IndexDepot - vBulletin 3.x Solr Search
+ * Copyright (c) 2012 IndexDepot
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * @copyright  IndexDepot 2012
+ * @author Vadims Karpuschkins
+ * @license LGPL
+ */
 require_once (dirname(__FILE__) . '/../../../Apache/Solr/HttpTransport/Abstract.php');
 require_once (dirname(__FILE__) . '/../../../Apache/Solr/HttpTransport/Response.php');
 
